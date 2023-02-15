1fe302396940a0fafcb0324882ce69fc526529fa
Abhishek Sanghavi <abhi.sanghavi@ggmail.com>
2023-02-15 01:07:51 +0000

    changes

---
+++ java/javawork/Class_X.java
@@ -0,0 +1,31 @@
+package javawork;
+
+//package name has changed from "java" to "javatopic"
+public class Class_X {
+    // class name has changed from "Class_1" to "Class_x "
+    public static void main(String[]args){
+
+        System.out.println("Hello world");
+        System.out.println("welcome to java");
+        System.out.println("my name is: Abhishek Sanghavi");
+
+        int age_a= 18;
+
+        System.out.println("my age: " +age_a);
+        System.out.println("we are learning software testing");
+
+        System.out.println("Registration: 1");
+        System.out.println("Registration: 2");
+        System.out.println("Registration: 3");
+        System.out.println("Registration: 4");
+        System.out.println("Registration: 5");
+        System.out.println("Thank you!!!");
+        System.out.println("Thank you!!!");
+
+
+
+
+
+
+    }
+}
