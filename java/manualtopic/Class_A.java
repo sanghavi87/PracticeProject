753d4220ee06f126bf5806fb7924acba28f2d768
Abhishek Sanghavi <abhi.sanghavi@ggmail.com>
2023-02-15 00:46:24 +0000

    Unify Homework

---
+++ java/manualtopic/Class_A.java
@@ -0,0 +1,26 @@
+package manualtopic;
+
+     // package name has changed from "manual" to "manualtopic"
+public class Class_A {
+    // class name has changed from "Class_2" to "Class_A"
+    public static void main(String[]args){
+
+        //days of the week
+
+        System.out.println("Monday");
+        System.out.println("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.println("Friday");
+        /*System.out.println("Saturday");
+        System.out.println("Sunday");*/
+
+
+    }
+
+
+
+
+
+
+}
